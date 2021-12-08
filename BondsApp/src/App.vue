2e0a0f46cf8b55fe94f64873f273151896f07540
Nikita Knyshev <55587189+PozhiloyGoblin@users.noreply.github.com>
2021-12-08 21:30:25 +0300

    Add files via upload

---
+++ BondsApp/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h2 class="title">Стоимость акций компаний</h2>
+    <Lab />
+  </div>
+</template>
+
+<script setup>
+import Lab from './components/Lab.vue'
+
+</script>
+
